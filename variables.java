607ae8cbd35221fef390787ae965eaa9923fb40a
Nizam0070 <clouddatabase007@gmail.com>
2026-06-30 16:41:11 +0530

    VARIABLES

---
+++ variables.java
@@ -0,0 +1,16 @@
+//variables
+class hello {
+    public static void main(String[] args) {
+      //incializing variables
+      int a=10;
+      int b=5;
+      String name ="nizam";
+      //performing  ADD operation
+      int sum = a+b;
+      System.out.println(sum);
+      //performing  MUL,DIV,ADD operation
+      int c = (a*b)/(a-b);
+      System.out.println(c);
+
+    }
+  }
