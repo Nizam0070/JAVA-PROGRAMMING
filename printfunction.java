95f9e550f43a171a08f60d03a3e7a726a16ffa2c
Nizam0070 <clouddatabase007@gmail.com>
2026-06-30 13:41:37 +0530

    print functions

---
+++ printfunction.java
@@ -0,0 +1,20 @@
+class hello {
+    public static void main(String[] args) {
+         System.out.print("*");
+          System.out.print("**");
+            System.out.print("***");
+              System.out.println("****");
+              System.out.println("*\n**\n***\n****");
+              System.out.print("*\n**\n***\n****");
+    }
+}
+//output//
+**********
+*
+**
+***
+****
+*
+**
+***
+****//
