8c1e80259ad89296bd3805fb8472785d1902ac95
Nizam0070 <clouddatabase007@gmail.com>
2026-07-01 18:09:45 +0530

    for,while,do while loops

---
+++ LOOPS.java
@@ -0,0 +1,17 @@
+//for loop//
+class hello{
+  public static void main(String[] args) {
+    //TABLE PRINTING//
+    //Scanner sc= new Scanner(System.in);
+    //int num =sc.nextInt();
+    //System.out.println("enter the num:");
+    //for (int i=1;i<=10;i++)
+    //{
+      ////System.out.println(num + " X " + i + " = " + (num*i));}
+      for (int i=1;i<10;i++)
+      {System.out.println(i);}
+  }
+}
+ 
+
+
