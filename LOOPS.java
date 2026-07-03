ffa2992029e1e9439b3fffe52a010c9f3c9b20d4
Nizam0070 <clouddatabase007@gmail.com>
2026-07-03 16:32:24 +0530

    Add while loop example to LOOPS.java

--- LOOPS.java
+++ LOOPS.java
@@ -12,6 +12,18 @@ class hello{
       {System.out.println(i);}
   }
 }
+
+//while//
+class hello{
+ public static void main(String args[]){
+  int i=0;
+  while (i<11){
+    System.out.println(i);
+    i++;
+  }
+  }
+}
+
  
 
 
