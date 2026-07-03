310e7c5017c454537bc450e455b99cbae74885e2
Nizam0070 <clouddatabase007@gmail.com>
2026-07-03 16:54:32 +0530

    FOR , WHILE, DO WHILE
    Added a do-while loop to print multiplication table.

--- LOOPS.java
+++ LOOPS.java
@@ -24,6 +24,25 @@ class hello{
   }
 }
 
+
+//do while//
+import java.util.*;
+class hello{
+ public static void main(String args[]){
+  Scanner sc =new Scanner(System.in);
+  System.out.print("enter a num :");
+  int num =sc.nextInt();
+   int i=1;
+  do { 
+    System.out.println(num+"x"+i+"="+(num*i));
+    i++;
+  }while(i<=10);
+  
+  }
+}
+
+
+
  
 
 
